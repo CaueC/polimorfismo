86350bac697601623c5b9dfe01024631db8a2466
CaueC <caue.coube@fatec.sp.gov.br>
2022-05-30 18:00:50 +0000

    salvando

---
+++ Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Quadrado quadrado= new Quadrado();
+        Quadrilatero quadrilatero= new Quadrilatero();
+        Retangulo retangulo= new Retangulo();
+        Losango losango= new Losango();
+        Trapezio trapezio= new Trapezio();
+        
+    
+        quadrado.calcularArea();
+        quadrilatero.calcularArea();
+        retangulo.calcularArea();
+        losango.calcularArea();
+        trapezio.calcularArea();
+
+        }
+    
+    
+}
---
+++ Quadrado.java
@@ -0,0 +1,14 @@
+public class Quadrado extends Quadrilatero {
+
+    @Override
+    public void calcularArea() {
+        System.out.println("L x L");
+
+    }
+
+
+    public Quadrado(){
+
+    }
+    
+}
